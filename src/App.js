95e408ca890c2f11617f5ffea7f1fdb302e9649d
Marius Christian Aastebøl <mariusca@hiof.no>
2023-09-16 18:09:54 +0200

    Done task 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import MyComponent from "./components/MyComponent";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+
 const App = () => (
   <div>
     <p>Read the README.md to see the tasks</p>
+    <MyComponent title="It works"/>
+    <Wrapper><Title title="Test" /></Wrapper><Title></Title>
   </div>
 );
 
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,4 @@
+import Title from "./Title"
+export default function MyComponent ({title}) {
+    return (<Title title={title}/>)
+}
